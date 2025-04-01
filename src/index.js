31f94ec95e2d4eebc56d845582a8dbe592903ed2
kika1s1 <tamiratkebede120@gmail.com>
2025-04-01 10:42:18 +0300

    feat: Add Express server with health check route

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import { createTelegramClient } from './services/telegramService.js';
 import { handleMessage } from './controllers/messageController.js';
 import { connectDatabase } from './services/databaseService.js';
 import { NewMessage } from 'telegram/events/index.js';
+import express from 'express';
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+// Health check route for UptimeRobot
+app.get('/live', (req, res) => {
+  res.json({"message":'Dude bot is alive!'});
+});
 (async () => {
   await connectDatabase();
   const client = await createTelegramClient();
@@ -11,3 +18,8 @@ import { NewMessage } from 'telegram/events/index.js';
   
   console.log('Bot is running and listening for messages...');
 })();
+
+// Start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
